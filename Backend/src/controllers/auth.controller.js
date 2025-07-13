433117fc138ec6e60616bba304ddf8a672ba680a
m-ahmad-amin <mahmadamindw@gmail.com>
2025-07-13 22:38:10 +0500

    Basic Setup of App and Database

---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+    res.send("sign up")
+};
+
+export const login =  (req, res) => {
+    res.send("login")
+}
+
+export const logout = (req, res) => {
+    res.send("log out")
+};
