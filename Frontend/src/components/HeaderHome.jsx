f101da0b8bcf54ed48c4f6ef35067b6ebc7ff4f3
m-ahmad-amin <mahmadamindw@gmail.com>
2025-07-19 11:03:17 +0500

    Style Homepage

--- Frontend/src/components/HeaderHome.jsx
+++ Frontend/src/components/HeaderHome.jsx
@@ -1,10 +1,19 @@
 import logo from "../assets/logo.png";
+import { Search } from "lucide-react";
 
 export default function HeaderHome() {
   return (
     <>
-      <div className="flex p-4 items-center md:p-0 md:justify-center">
+      <div className="flex justify-between p-4 items-center md:p-0 md:justify-center">
         <img src={logo} alt="Linracy" className="w-2/5 h-auto md:w-[30%]" />
+        <div className="flex items-center gap-3 md:hidden">
+            <Search className="text-gray-500 w-10 h-10" />
+            <img
+          src="https://res.cloudinary.com/dzzrxqiho/image/upload/v1752450074/samples/two-ladies.jpg"
+          alt="profile pic"
+          className="object-cover rounded-full w-12 h-12"
+        ></img>
+        </div>
       </div>
     </>
   );
